2b0f101fed494ec1dfbd1c9a40ce47f4f88abd58
Luiz carlos Godoy <02016891629@pucminas.net>
2026-05-21 18:59:28 -0300

    alterações do dia 21

--- cadastro.js
+++ cadastro.js
@@ -1,43 +1,45 @@
-let nome = document.getElementById("nome")
-let Lista_de_jogos = document.querySelector(".Lista_de_jogos")
-let descrição = getElementById(descrição)
-let jogos = [];
+let descricao = document.querySelector("#descricao");
+let nome = document.querySelector("#nome");
+let link = document.querySelector("#link");
+let input_com_imagem = document.getElementById("input_com_imagem");
+let lista_de_jogos = document.querySelector(".lista_de_jogos")
 
-
-function cadastrar_jogo() {
-    novo_jogo = Jogo(nome.value, descrição.value)
-    jogos.push(novo_jogo);
-    console.log(novo_jogo);
-
-    Lista_de_jogos.innerHTML = "";
-
-    // jogos.map = (function (_jogo, posição_array) {
-    //    const div = document.createElement("div")
-    //     div.innerHTML =
-    //         `
-    //    <p>${jogos[contadora].nome}</p>
-    //    <p>${jogos[contadora].descrição}</p>
-    // `
-    //     Lista_de_jogos.appendChild(div)
-    // })
-}
-   
-for (let i = 0; i < jogos.length; i++){
-    const nome = document.createElement("h3");
-    const descrição = document.createElement("p");
-
-    nome.textContent = jogos[i].nome;
-    descrição.textContent = jogos[1].descrição;
-
-    Lista_de_jogos.appendChild(nome)
-    Lista_de_jogos.appendChild(descrição)
-}
-
-
-
-const Jogo = (nome, descrição) => {
+const Jogo = (nome, descricao, imagem, link) => ({
     nome,
-        descrição
+    descricao,
+    imagem,
+    link,
+});
+
+const jogos = [];
+function PegarURLImagem() {
+    if (input_com_imagem.files.length === 0) {
+        return "";
+    }
+    return URL.createObjectURL(input_com_imagem.files[0]);
 }
 
 
+function CriarJogo() {
+    const urlImagem = PegarURLImagem();
+    const novoJogo = Jogo(nome.value, descricao.value, urlImagem, link.value)
+    jogos.push(novoJogo);
+
+    lista_de_jogos.innerHTML = "";
+    for (let i = 0; i < jogos.length; i++){
+        const div = document.createElement("div");
+        div.innerHTML = `
+    <h3>${jogos[i].nome}</h3>
+    <p>${jogos[i].descricao}<p/>
+    <a href = "${jogos[i].link}" target= _"blank">Link do jogo</a>
+    <img width = "100px" src = "${jogos[i].imagem}">
+    <button onclick = "RemoverElemento(${i})">
+    Remover</button>
+    `
+    lista_de_jogos.appendChild(div);
+}}
+   
+
+function RemoverElemento(indice){
+    jogos.splice(indice, 1)
+}
